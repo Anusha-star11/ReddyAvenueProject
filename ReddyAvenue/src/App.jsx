845087ebae08ba18eb5da4615a87a5ca13299999
Anusha-star11 <anushapoosala11@gmail.com>
2024-07-16 12:58:46 +0530

    create pages and routes

--- ReddyAvenue/src/App.jsx
+++ ReddyAvenue/src/App.jsx
@@ -1,15 +1,24 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import SignIn from "./Pages/SignIn"
+import SignUp from "./Pages/SignUp"
+import Dashboard from "./Pages/Dashboard"
 
 function App() {
   
 
   return (
-    <>
-      <div className="text-red-500 text-3xl">
-        Hello
-              </div>
-      
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
