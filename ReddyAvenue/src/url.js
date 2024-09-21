59f7dc725f3df8b8bc26a0a30d87b7a102dc4a2b
Anusha-star11 <anushapoosala11@gmail.com>
2024-09-21 23:18:36 +0530

    final2

--- ReddyAvenue/src/url.js
+++ ReddyAvenue/src/url.js
@@ -1,4 +1,4 @@
 const baseURL = import.meta.env.PROD 
-  ? 'https://reddyavenueproject.onrender.com/'  // Production URL
+  ? 'https://reddyavenueproject.onrender.com'  // Production URL
   : 'http://localhost:3147'; // For local development
 export default baseURL;
